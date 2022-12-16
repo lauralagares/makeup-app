8c94e505a64528791bc6b01004d6d02fbd2ca577
lauralagares <lauralagares1992@gmail.com>
2022-12-16 15:45:27 +0100

    fix

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,24 +1,24 @@
 import './Style.css';
 import { Card } from 'react-bootstrap';
-import {useContext} from 'react';
+import { useContext } from 'react';
 import { CartContext } from '../../Context/Cart.Context';
 import Soldout from '../../Images/sold-out.png';
 
 function ProductCard({ product }) {
 
-  let {addProduct, isInCart} = useContext(CartContext);
+  let { addProduct, isInCart } = useContext(CartContext);
   let productImage = product.image_link;
 
   let productName = product.name.split(" ", 3);
   let shortProductName = productName.join(" ");
-  
+
   const handleError = (e) => {
     e.target.src = Soldout;
     e.onError = null;
   }
-  
+
   const handleAdd = () => {
-    addProduct(product,1)
+    addProduct(product, 1);
   }
 
   return (
@@ -27,25 +27,25 @@ function ProductCard({ product }) {
         onError={handleError} alt={product.name} />
       <section className='card-body-container'>
 
-          <p className='text-capitalize text-center'> {shortProductName}</p>
-    
-          <p className="text-uppercase">{product.brand}</p>
+        <p className='text-capitalize text-center'> {shortProductName}</p>
 
-          <p>{product.price}€</p>
+        <p className="text-uppercase">{product.brand}</p>
 
-          <div className='colors-info'>
-            {product.product_colors.slice(0,4).map((c, i) =>
-              <div key={i}
-              style={{backgroundColor: c.hex_value}}></div>)
-            }
-          </div>
+        <p>{product.price}€</p>
 
-          {
-            isInCart(product.id) === true
+        <div className='colors-info'>
+          {product.product_colors.slice(0, 4).map((c, i) =>
+            <div key={i}
+              style={{ backgroundColor: c.hex_value }}></div>)
+          }
+        </div>
+
+        {
+          isInCart(product.id) === true
             ? <button className="button-add-2"><span>ADDED TO CART</span></button>
             : <button className="button-add-1" onClick={handleAdd}><span>ADD TO CART</span></button>
-          }
-  
+        }
+
       </section>
     </Card>
   )
--- src/Context/Cart.Context.js
+++ src/Context/Cart.Context.js
@@ -1,32 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
 
-    const [cart, setCart] = useState([]); 
+    const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart') || "[]"));
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart])
 
     const addProduct = (item, quantity) => {
         if (isInCart(item.id)) {
             setCart(cart.map(product => {
-                return product.id === item.id ? {...product, quantity: quantity + 1} : product
+                return product.id === item.id ? { ...product, quantity: quantity + 1 } : product
             }))
         } else {
-            setCart([...cart, {...item, quantity}])
+            setCart([...cart, { ...item, quantity }])
         }
     }
 
     const substractProduct = (item, quantity) => {
         if (isInCart(item.id)) {
             setCart(cart.map(product => {
-                return product.id === item.id ? {...product, quantity: quantity - 1} : product
+                return product.id === item.id ? { ...product, quantity: quantity - 1 } : product
             }))
         } else {
-            setCart([...cart, {...item, quantity}])
+            setCart([...cart, { ...item, quantity }])
         }
     }
 
-    const isInCart = (id) => { 
+    const isInCart = (id) => {
         return cart.find(product => product.id === id) ? true : false;
     }
 
@@ -35,13 +39,12 @@ const CartProvider = ({ children }) => {
     }
 
     const totalProducts = () => {
-        return cart.reduce((prev,act) =>  prev + act.quantity, 0)
+        return cart.reduce((prev, act) => prev + act.quantity, 0)
     }
 
     const totalPrice = () => {
-        return cart.reduce((prev,act) => prev + act.quantity * act.price , 0)
+        return cart.reduce((prev, act) => prev + act.quantity * act.price, 0)
     }
-
 
     return <CartContext.Provider value={{
         isInCart,
